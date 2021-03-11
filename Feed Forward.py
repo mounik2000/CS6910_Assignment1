357e122cdcab31596399acf4f6625420a4636faf
PragnyaAnuboina <cs17b004@smail.iitm.ac.in>
2021-03-11 15:03:40 +0530

    Added feedforward neural network code(Q2)

--- Feed Forward.py
+++ Feed Forward.py
@@ -2,7 +2,7 @@ from keras.datasets import fashion_mnist
 import numpy as np
 import wandb
 
-
+#Question 1
 def plot_sample_images():
     (X_train, Y_train), (X_test, Y_test) = fashion_mnist.load_data()
     classes = []
@@ -20,6 +20,41 @@ def plot_sample_images():
         if(len(visited) == 10):
             break
     wandb.log({"Examples for each class": Images})
-    
-    
-plot_sample_images()
+        
+
+# Question 2
+#dimensions : 
+#X_input -> n
+#a_i,h_i : (ith hidden_layer_size)
+#W_list[i] : (ith hidden_layer_size) x ({i-1}th hidden_layer_size)
+#b_list[i] : (ith hidden_layer_size)
+#Output : h_list (h_1,h_2....h_{num_hidden_layers}), a_list (a_1,a_2....a_{num_hidden_layers+1})), y
+def feed_forward_neural_network(X_input, W_list, b_list, activation_function, num_hidden_layers, hidden_layer_size) :
+  
+    X_input = X_input.reshape(-1,1)
+    h_prev = X_input
+    a_i = []
+    h_list = []
+    a_list = []
+    #Other than o/p layer
+    for i in range(0, num_hidden_layers) :
+        a_i = b_list[i].reshape(-1,1) + np.dot(W_list[i], h_prev)
+        a_list.append(a_i)
+        if activation_function == 'sigmoid' :
+            h_prev = 1 /( 1 + np.exp(-1 * a_i))
+        elif activation_function == 'tanh' :
+            h_prev = np.tanh(a_i)
+        elif activation_function == 'ReLU' :
+            h_prev = np.maximum(0, a_i)
+        h_list.append(h_prev)
+    a_i = b_list[num_hidden_layers].reshape(-1,1) + np.dot(W_list[num_hidden_layers], h_prev)    
+    #soft_max
+    a_list.append(a_i)
+    a_i = np.exp(a_i)
+    summ = np.sum((a_i))
+    y = a_i / summ
+    parameters = []
+    parameters.append(h_list)
+    parameters.append(a_list)
+    parameters.append(y)
+    return parameters
